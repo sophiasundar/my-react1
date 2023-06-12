2be1e0d7017c8a26c6efa18b7fade11299f6e191
sophiasundar <sophiagifta17@gmail.com>
2023-06-12 12:44:46 +0530

    commit

---
+++ src/Components/Firstcode.js
@@ -0,0 +1,12 @@
+const Firstcode=()=>{
+     const name="sophia"
+     const time=30
+    return(
+        <div>
+            <h1>Firstcode-React</h1>
+            <h2>Time take to complete 1 task by {name} is {time*4}mins</h2>
+        </div>
+    )
+}
+
+export default Firstcode; //export
---
+++ src/Components/Message.js
@@ -0,0 +1,9 @@
+const Message=()=>{
+    return(
+        <div>
+            <h2>Message Component</h2>
+        </div>
+    )
+}
+
+export default Message
