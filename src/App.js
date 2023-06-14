7be206384128161e40d7bcd6c59f52ca59473c98
sophiasundar <sophiagifta17@gmail.com>
2023-06-14 12:54:55 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import { Component } from 'react';
-// import Firstcode from './Components/Firstcode';
-// import Message from './Components/Message';
+// import { Component } from 'react';
+import Firstcode from './Components/Firstcode';
+
 
 function App() { 
   return (
     <div className="App">
       <h1>Hello World</h1>
       <h2>Welcome to React</h2>
-       
+       <Firstcode></Firstcode>
        {/* <Message Nicname="Guvi" image="https://tse3.mm.bing.net/th?id=OIP.DwtwUF50gl3Ue_YXPHvi7AHaDL&pid=Api&P=0&h=180"/>
        <RepeatMessage Nicname="Guvi" image="https://tse3.mm.bing.net/th?id=OIP.DwtwUF50gl3Ue_YXPHvi7AHaDL&pid=Api&P=0&h=180"/> */}
     
